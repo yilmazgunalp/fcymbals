47f30b11772471cd922fe82beb6d18c2ba424c93
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-11-05 22:31:37 +1100

    trying heroku env vars

--- app/helpers/retailers_helper.rb
+++ app/helpers/retailers_helper.rb
@@ -1,6 +1,6 @@
 module RetailersHelper
 
-$log_file = File.new("#{Rails.root}/log/scrape_log.txt")
+# $log_file = File.new("#{Rails.root}/log/scrape_log.txt")
 $models = YAML.load_file("#{Rails.root}/db/models.yml")
 $brands = $models.keys
 $types = Maker.select(:kind).distinct.map {|m| m.kind}
