b608095b775bac2e296b8b6c09e0e33a06bb1816
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-11-05 22:57:53 +1100

    add jobs to makers index

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,6 +1,9 @@
 class MakersController < ApplicationController
   def index
   	@cymbals = Maker.all
+  	Resque.enqueue_at(60.seconds.from_now,TestingJob)
+	Resque.enqueue(SleepingJob)
+ Resque.enqueue(CleanupJob,3699)
   	
   end
 end
