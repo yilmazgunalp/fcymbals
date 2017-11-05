3a99e0795d3c495eb64a4ca166dbb00e8e28087c
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-11-05 22:52:34 +1100

    changed cleanup job

--- app/jobs/cleanup_job.rb
+++ app/jobs/cleanup_job.rb
@@ -1,12 +1,12 @@
 class CleanupJob 
 	@queue = :myq 
 
-    @@file = File.new("#{Rails.root}/log/deneme.txt")
+    
   def self.perform(m)
    puts "<-------START JOB---------->"
    puts "<----I AM A JOB------------------>"
    puts "<-------------------------------->"
-   File.open(@@file, 'a+') {|f| f.write m }
+  
    p Maker.find(m).dup
    puts "<--------END JOB------------->"
   end
