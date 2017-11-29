9fe868ea6a7ced07e34f0433be6a10f98de51dce
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-11-30 00:11:03 +1100

    hmm trying again

--- app/shops/kosmic.rb
+++ app/shops/kosmic.rb
@@ -14,6 +14,7 @@ file <<  ["title","price","s_price","picture_url","merchant","link","description
 
 def self.scrape 
 puts "inside kosmic scrape now"	
+p File.atime(size)
 param = 1 	
 page = get_page(shop['url'],{"pgnum" => param})
 
@@ -22,7 +23,8 @@ page = get_page(shop['url'],{"pgnum" => param})
 	param += 1
 	page = get_page(shop['url'],{"pgnum" => param})
 	end # while	
-p File.atime(file)	
+p File.atime(file)
+p File.atime(size)
 end #scrape()
 
 
