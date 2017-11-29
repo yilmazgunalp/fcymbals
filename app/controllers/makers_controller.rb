f42a0ec3faebbaaf2b2ab2d6b1ede0908cec0921
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-11-29 22:06:54 +1100

    fix maker index

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,9 +1,5 @@
 class MakersController < ApplicationController
   def index
   	@cymbals = Maker.all
-  	Resque.enqueue_at(60.seconds.from_now,TestingJob)
-	Resque.enqueue(SleepingJob)
- Resque.enqueue(CleanupJob,3699)
-  	
   end
 end
