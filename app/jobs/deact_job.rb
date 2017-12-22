8384642591bc185ce055f58b507226d7176e8efb
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-12-22 23:37:31 +1100

    fix typo

--- app/jobs/deact_job.rb
+++ app/jobs/deact_job.rb
@@ -8,7 +8,7 @@ def self.queue
     Retailer.deactivate_records(merchant,Time.now+5.minutes)
     log_file = File.open("#{Rails.root}/tmp/Retailer_Deact_log.txt","a+")
     Retailer.where("merchant = ? AND updated_at > ? AND active = ? ",merchant,Time.now-15.minutes,false).each do |r|
-     log_file << r.to_log +"\n" 
+     log_file << r.to_log + "\n" 
     end 
     Resque.enqueue(MaillogJob,File.path(log_file),merchant,:deactivated)
   end
