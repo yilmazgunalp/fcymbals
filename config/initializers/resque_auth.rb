7e34f6d4edfa232c598a49731dc0b142085e613c
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2017-12-26 22:39:10 +1100

    add removed sens files

---
+++ config/initializers/resque_auth.rb
@@ -0,0 +1,10 @@
+Resque::Server.use(Rack::Auth::Basic) do |user,password|
+
+password == ENV["RESQUE_WEB_PSWD"]
+
+end	
+
+Resque.logger.formatter = Resque::VeryVerboseFormatter.new
+Resque.logger.level = Logger::DEBUG
+
+Resque.schedule = YAML.load_file("#{Rails.root}/app/jobs/_schedule.yml")
