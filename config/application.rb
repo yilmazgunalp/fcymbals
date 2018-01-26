1b83bc6ce4d02ac8ebb138691b661936548e41eb
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2018-01-26 18:23:38 +1100

    more work

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Fcymbals
   class Application < Rails::Application
-  	config.autoload_paths += %W(#{config.root}/lib/allocate)
-  	config.autoload_paths += %W(#{config.root}/lib/scrape)
+  	config.autoload_paths += %W(#{config.root}/lib/allocate/)
+  	config.autoload_paths += %W(#{config.root}/lib/scrape/)
   	# Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
