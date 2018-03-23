ad2da4fef2b3dbd3fdf9ced97834fd5a1dd20d24
Yilmaz Gunduzalp <yilmazgunalp@gmail.com>
2018-03-23 12:28:59 +1100

    testing deact job

--- app/jobs/deact_job.rb
+++ app/jobs/deact_job.rb
@@ -7,7 +7,7 @@ def self.queue
   def self.perform merchant
     #This job was queued 1 hour ago so give another hour as buffer and deactivae records if they
     #have not been updated in the last two hours
-    Retailer.deactivate_records(merchant,Time.now-2.hours)
+    Retailer.deactivate_records(merchant,Time.now-1.hours)
     log_file = File.open("#{Rails.root}/tmp/Retailer_Deact_log.txt","a+")
     #Get all the records that has been updated in the last 15 minutes and inactive and write them  to log
     #
