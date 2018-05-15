e0f024eed7aa6e46c0d601125038fb82353e3d18
yilmazgunalp <yilmazgunalp@gmail.com>
2018-05-15 22:34:39 +1000

    Fix solr string match regex 3

--- app/solr/solr.rb
+++ app/solr/solr.rb
@@ -25,7 +25,7 @@ module Solr
 		def match(retailers)
 			result = {}	
 			retailers.each do |r|
-				rq = r.title.gsub(/"”/,"").match?(/(set)|(pack)/) ? "cymbalsets" : "allocate"
+				rq = r.title.gsub(/["”]/,"").match?(/(set)|(pack)/) ? "cymbalsets" : "allocate"
 				useparams = rq == "allocate" ? "alloc" : "cymbalsets"
 				uri = parse_string(rq,{q: encode_ascii(r.title), 
 					wt: 'ruby', useParams: useparams, bf: boostfunction(useparams),sow: 'true'})
